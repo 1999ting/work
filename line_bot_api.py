cad7d5398f18d973e19a4c89a922543125a636d6
1999ting <139091172+1999ting@users.noreply.github.com>
2023-08-16 14:12:30 +0800

    Add files via upload

--- line_bot_api.py
+++ line_bot_api.py
@@ -1,12 +1,9 @@
-#載入LineBot所需要的套件
 from flask import Flask, request, abort
-from linebot import(LineBotApi, WebhookHandler, exceptions)
+from linebot import (LineBotApi, WebhookHandler, exceptions)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
-app = Flask(__name__)
-
-#Channel access token #在line Developers的providers的Basic settings
-line_bot_api = LineBotApi('Fgm1PZljto93lSH0NGfZcisx1YapoloDzRIcm4yUpWRt1luWNX+Ap6A1IcFGGOHpFOKRhMJ5olMurHOH1ylvR3fkOKsxR29KxeURfRj0/vRlZoHdylq+Vt+DQPAyE6vU4pOtZIkh0YTnZniBPYxrKAdB04t89/1O/w1cDnyilFU=')
-#Channel secret  #在line Developers的providers的Messaging API
-handler = WebhookHandler('b4d0c49e7ac03e0fdcc331ab66600c0e')
+# 必須放上自己的Channel Access Token
+line_bot_api = LineBotApi('92kcy07IM75HrTqA3x+nCn61QmpKr0I5c1gH3Dumn5MR8DP1876/38wqBFlF/1KqP+R+cjzpqvxLD1NELcik69B92V8+k4Wt0ZroHxEoL5HZ19/nT7RC6v3j08G6AFd2/sFxfJy2tsy84WGEbNNrwQdB04t89/1O/w1cDnyilFU=')
+# 必須放上自己的Channel Secret
+handler = WebhookHandler('8cfd13f4a443434527574c3c6b80b0ab')
